982c402536e2f61fa4f7760d127aba8f7a8cd81e
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2024-07-18 09:49:51 +0200

    18/07/2024

--- ejerciciosMios/holaMundo.cpp
+++ ejerciciosMios/holaMundo.cpp
@@ -1,58 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <list>
 
 using namespace std;
 
-int miAlgoritmo(vector<int> A){
-    int i=1;
-    int j=1;
-    int m=0;
-    int c=0;
-    while(i<=A.size()){
-        if(A[i]==A[j]) c++;
-        j++;
-        if(j > A.size()){
-            if(c > m) m = c;
-            c = 0;
-            i++;
-            j = i;
-        }
-    }
-    return m;
-}
 
-int miAlgoritmo2(vector<int> & A){
-    if(A.size() == 0) return 0;
-    sort(A.begin(), A.end());
-
-    for(int i : A) cout << i << " ";
-    cout << "\n";
-
-    int max = -1;
-    int contador = 1;
-    int anterior = A[0];
-    for(int i=1;i<A.size();i++){
-        if(anterior == A[i]) contador++;
-        else{
-            max = contador > max ? contador : max;
-            contador = 1;
-            anterior = A[i];
-        }
-    }
-    max = contador > max ? contador : max;
-    return max;
-}
-
-void imprimirArray(vector<int> & array){
-    for(int i : array) cout << i << " ";
-    cout << "\n";
-}
 
 int main(){
-    vector<int> array = {4, 1, -5, 7, 2, 9};
-    sort(array.begin(), array.end()); 
-    imprimirArray(array); // -5 1 2 4 7 9
-    sort(array.rbegin(), array.rend()); // 9 7 4 2 1 -5
-    imprimirArray(array);
+    list<int> numeros = {4, 5, 6, 7};
+    numeros.push_back(9);
+    numeros.push_front(1);
+
+    while(!numeros.empty()){
+        cout << numeros.front() << "\n";
+        cout << numeros.back() << "\n";
+        numeros.pop_front();
+        numeros.pop_back();
+    }
+
+    for(int i : numeros){
+        cout << i << "\n";
+    }
+
 }
